3c388756026d3da1505970a254b5825af62a5b5c
Michael Rios <riosmichael28@ymail.com>
2020-02-05 18:33:28 -0800

    added grid/node movement; performance is kinda crappy

--- src/components/PathfindingVisualizer.js
+++ src/components/PathfindingVisualizer.js
@@ -8,9 +8,9 @@ import swap from "lodash-move"
 import "./styles.css"
 
 const Node = styled(animated.div)`
-   width: 20px;
-   height: 20px;
-   background-color: pink;
+   width: 30px;
+   height: 30px;
+   background-color: #eee;
    margin: 2px;
    border-radius: 3px;
    position: absolute;
@@ -23,86 +23,62 @@ const Grid = styled.div`
    height: 100vh;
 `
 
-// Returns fitting styles for dragged/idle items
-const fn = (order, down, originalIndex, curIndex, y) => index => {
-   return down && index === originalIndex
-      ? {
-           y: curIndex * 25 + y,
-           scale: 1.1,
-           zIndex: "1",
-           shadow: 15,
-           immediate: n => n === "y" || n === "zIndex"
-        }
-      : {
-           y: order.indexOf(index) * 25,
-           scale: 1,
-           zIndex: "0",
-           shadow: 1,
-           immediate: false
-        }
+const layout = i => {
+   const row = Math.floor(i / 30)
+   const col = i % 30
+   return [35 * col, 35 * row]
 }
+
+// Returns fitting styles for dragged/idle items
+const fn = (order, down, originalIndex, curIndex, x, y) => index => {
+   if (down && index === originalIndex) {
+      let [tempx, tempy] = layout(curIndex)
+      return {
+         x: tempx + x,
+         y: tempy + y,
+         scale: 1.1,
+         zIndex: "1",
+         immediate: false
+      }
+   }
+   let [tempx, tempy] = layout(order.indexOf(index))
+   return { x: tempx, y: tempy, scale: 1, zIndex: "0", immediate: false }
+}
+
 export const PathfindingVisualizer = () => {
-   // temporary create grid
-   let grid = []
-   // rows
-   for (let x = 0; x < 10; x++) {
-      grid[x] = [] // inner array
-      //cols
-      for (let y = 0; y < 3; y++) {
-         grid[x][y] = {
-            f: 0,
-            g: 0,
-            h: 0,
-            parent: null,
-            isWall: false,
-            pos: `${x}, ${y}`
-         }
-      }
-   }
+   let items = new Array(200).fill("")
+   const order = useRef(items.map((_, index) => index)) // Store indicies as a local ref, this represents the order of the array
+   const [springs, setSprings] = useSprings(items.length, fn(order.current)) // Create springs, each corresponds to an item, controlling its transform, scale, etc.
 
-   grid = flatten(grid) // make 2D array =>  1D array
-   const order = useRef(grid.map((_, index) => index))
-   console.log("order", order.current)
-
-   const [springs, setSprings] = useSprings(grid.length, fn(order.current)) // Create springs, each corresponds to an item, controlling its transform, scale, etc.
-
-   // gesture ------------------
-   const bind = useGesture({
-      onDrag: ({ args: [originalIndex], down, movement: [, y] }) => {
-         const curIndex = order.current.indexOf(originalIndex)
-         const curRow = clamp(
-            Math.round((curIndex * 25 + y) / 25),
-            0,
-            grid.length - 1
-         )
-         const newOrder = swap(order.current, curIndex, curRow)
-         setSprings(fn(newOrder, down, originalIndex, curIndex, y)) // Feed springs new style data, they'll animate the view without causing a single render
-         if (!down) order.current = newOrder
-      }
+   const bind = useDrag(({ args: [originalIndex], down, movement: [x, y] }) => {
+      const curIndex = order.current.indexOf(originalIndex)
+      let [tempx, tempy] = layout(curIndex)
+      const curCol = clamp(Math.round((tempx + x) / 35), 0, 30 - 1)
+      const curRow = clamp(
+         Math.round((tempy + y) / 35),
+         0,
+         Math.floor(items.length / 30)
+      )
+      const index = curRow * 30 + curCol
+      const newOrder = swap(order.current, curIndex, index)
+      setSprings(fn(newOrder, down, originalIndex, curIndex, x, y)) // Feed springs new style data, they'll animate the view without causing a single render
+      if (!down) order.current = newOrder
    })
-   // ---------------- gesture
-
-   const handleClick = e => {
-      console.log("grid", grid)
-      console.log("order", order)
-   }
 
    return (
       <React.Fragment>
-         <VisualizerHeader handleClick={handleClick} />
+         <VisualizerHeader />
          <Grid className="grid">
-            {springs.map(({ zIndex, shadow, y, scale }, i) => (
+            {springs.map(({ zIndex, x, y, scale }, i) => (
                <Node
-                  {...bind(i)}
+                  {...(i === 1 ? { ...bind(i) } : null)}
                   key={i}
                   style={{
                      zIndex,
-                     boxShadow: shadow.interpolate(
-                        s => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`
-                     ),
                      transform: interpolate(
-                        [y, scale],
-                        (y, s) => `translate3d(0,${y}px,0) scale(${s})`
+                        [x, y, scale],
+                        (x, y, scale) =>
+                           `translate3d(${x}px,${y}px,0) scale(${scale})`
                      )
                   }}
                />
