9279a85fa1278a9c6a1830cbadd96df5a017a941
Michael Rios <riosmichael28@ymail.com>
2020-02-25 19:38:07 -0800

    remove tests

--- src/__tests__/create_grid.test.js
+++
@@ -1,7 +0,0 @@
-import { aStarAlgo } from "../algorithms/a_star_algorithm";
-
-test("creates grid correctly", () => {
-   expect(aStarAlgo.grid.length).toEqual(0);
-   aStarAlgo.init();
-   expect(aStarAlgo.grid.length).toEqual(80);
-});
